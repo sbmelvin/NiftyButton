95243c6706349d02a449d2a3b83c9924b3f64963
Stephen Melvin <stephenbmelvin@gmail.com>
2012-12-29 00:04:01 -0600

    - Simplified class interface. - Users can now set the color properties of the various UIControlStates individually. - Added the 'theme' property. The color theme can now be set via interface builder's "User Defined Runtime Attributes". KeyPath=theme, Type=String, Value={white,black,red,blue,green,yellow}.

--- NiftyButton.h
+++ NiftyButton.h
@@ -1,7 +1,3 @@
-
-// Based on the CBHybrid class available from: https://github.com/kaishin/custom-UIButton
-
-//
 //  NiftyButton.h
 //
 //  Created by Stephen Melvin <stephenbmelvin@gmail.com> on 12/16/2012.
@@ -21,25 +17,10 @@ typedef NS_ENUM(NSUInteger, NiftyButtonColorTheme)
 @class UIButton;
 @interface NiftyButton : UIButton
 
-@property (nonatomic) UIColor *normalBottomColor;
-@property (nonatomic) UIColor *normalTopColor;
-@property (nonatomic) UIColor *normalBorderColor;
-@property (nonatomic) UIColor *highlightedBottomColor;
-@property (nonatomic) UIColor *highlightedTopColor;
-@property (nonatomic) UIColor *highlightedBorderColor;
-@property (nonatomic) UIColor *selectedBottomColor;
-@property (nonatomic) UIColor *selectedTopColor;
-@property (nonatomic) UIColor *selectedBorderColor;
-@property (nonatomic) UIColor *disabledBottomColor;
-@property (nonatomic) UIColor *disabledTopColor;
-@property (nonatomic) UIColor *disabledBorderColor;
-
 + (NiftyButton*)buttonWithBorderColor:(UIColor*)aBorderColor topColor:(UIColor*)aTopColor bottomColor:(UIColor*)aBottomColor frame:(CGRect)aFrame;
-
-+ (NiftyButton*)buttonWithTheme:(NiftyButtonColorTheme) frame:(CGRect)aFrame;
++ (NiftyButton*)buttonWithTheme:(NiftyButtonColorTheme)colorTheme frame:(CGRect)aFrame;
 
 - (void)setButtonBorderColor:(UIColor*)aBorderColor topColor:(UIColor*)aTopColor bottomColor:(UIColor*)aBottomColor forState:(UIControlState)aState;
-
-- (void)setColorTheme:(NiftyButtonColorTheme)aTheme forState:(UIControlState)aState;
+- (void)setColorTheme:(NiftyButtonColorTheme)aTheme;
 
 @end
